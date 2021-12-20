2c7e7e62c733704e5260e926529a4a3c1c260849
hanna-rosenberg <90154790+hanna-rosenberg@users.noreply.github.com>
2021-12-20 14:34:33 +0100

    uploaded images works, password is hashed

--- app/users/register.php
+++ app/users/register.php
@@ -11,12 +11,16 @@ require __DIR__ . '/../autoload.php';
 //     $message = 'The file was successfully uploaded!';
 // }
 
+
+// om man fyller i användarnamn, email, lösenord och fullname filtreras, sanitizas, trimmas (och lösenordet hashas) 
+// det du skrivit in och sparas i variabler. 
 if (isset($_POST['username'], $_POST['email'], $_POST['password'], $_POST['full-name'])) {
     $username = trim(filter_var($_POST['username'], FILTER_SANITIZE_STRING));
     $email = trim(filter_var($_POST['email'], FILTER_SANITIZE_STRING));
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
     $fullname = trim(filter_var($_POST['full-name'], FILTER_SANITIZE_STRING));
 
+    // här läggs det inskriva in i databasen på ett säkrare sätt med hjälp av tomma "placeholders". 
     $sql = "INSERT INTO users (nickname, email, name, password) VALUES (':username', ':email', ':fullname', ':password')";
 
     $statement = $database->prepare("INSERT INTO users (nickname, email, name, password) VALUES (:username, :email, :fullname, :password)");
@@ -25,6 +29,7 @@ if (isset($_POST['username'], $_POST['email'], $_POST['password'], $_POST['full-
     $statement->bindParam(':password', $password, PDO::PARAM_STR);
     $statement->bindParam(':fullname', $fullname, PDO::PARAM_STR);
 
+    // det hela körs. 
     $statement->execute();
 }
 
