846cc93964a87c9a045bba4eda8008329b82140b
hanna-rosenberg <90154790+hanna-rosenberg@users.noreply.github.com>
2021-12-28 16:38:40 +0100

    date in task to datebase

--- app/users/tasks.php
+++ app/users/tasks.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
+
 if (isset($_POST['task'])) {
     $task = trim(filter_var($_POST['task'], FILTER_SANITIZE_STRING));
 
@@ -12,6 +13,23 @@ if (isset($_POST['task'])) {
 
     $statement = $database->prepare("INSERT INTO tasks (description) VALUES (:placeholderTask)");
     $statement->bindParam(':placeholderTask', $task, PDO::PARAM_STR);
+
+
+    // det hela körs. 
+    $statement->execute();
+}
+
+if (isset($_POST['task'], $_POST['date'])) {
+    $task = trim(filter_var($_POST['task'], FILTER_SANITIZE_STRING));
+    $date = ($_POST['date']);
+
+
+    // här läggs det inskriva in i databasen på ett säkrare sätt med hjälp av tomma "placeholders". BEHÖVS DENNA?
+    // $sql = "INSERT INTO users (nickname, email, name, password) VALUES (':username', ':email', ':fullname', ':password')";
+
+    $statement = $database->prepare("INSERT INTO tasks (description, deadline) VALUES (:placeholderTask, :placeholderDeadline)");
+    $statement->bindParam(':placeholderTask', $task, PDO::PARAM_STR);
+    $statement->bindParam(':placeholderDeadline', $date, PDO::PARAM_INT);
 
 
     // det hela körs. 
--- tasks.php
+++ tasks.php
@@ -6,14 +6,19 @@
     <p>This is the tasks page.</p>
 
 
-    <form action="app/users/tasks.php" method="post" enctype="multipart/form-data">
+    <form action="app/users/tasks.php" method="post">
 
         <div class="mb-3">
             <label for="task">task</label>
             <input class="form-control" type="task" name="task" id="task" placeholder="write a task" required>
-            <small class="form-text">Please provide a desired task</small>
+
+
+            <label for="date">date</label>
+            <input class="date" type="date" name="date" id="date" placeholder="pick a date" required>
+
+
+            <button type="submit" class="submitTask">Add</button>
         </div>
-        <button type="submit" class="btn btn-dark">Add</button>
     </form>
 
 </article>
